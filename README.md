61c96e782ff6947c61cbfa9b17e90f48373b5b29
sako kazuhiro <sako@rio.ne.jp>
2019-01-22 03:11:03 +0900

    add eslint

--- README.md
+++ README.md
@@ -23,3 +23,15 @@ bin/webpack-dev-server
 ### master 以外のブランチデプロイ
 https://qiita.com/wroc/items/d15b1015c899b0cf77da
 git push heroku heroku-staging:master --force
+
+### eslint
+
+#### 構文チェック
+```
+$ bundle exec yarn run eslint 対象ファイルのパス
+```
+
+#### 構文チェック & 自動整形
+```
+$ bundle exec yarn run eslint --fix 対象ファイルのパス
+```
--- app/javascript/packs/dentists.js
+++ app/javascript/packs/dentists.js
@@ -1,21 +1,21 @@
-$(function(){
+$(() => {
   // 住所
-  let location = { lat: 35.632896, lng: 139.880394 }
-  let map = new google.maps.Map(document.getElementById('map'), { zoom: 15, center: location })
+  const location = { lat: 35.632896, lng: 139.880394 }
+  const map = new google.maps.Map(document.getElementById('map'), { zoom: 15, center: location })
 
-  //google mapsインスタンス化
-  let transitLayer = new google.maps.TransitLayer()
+  // google mapsインスタンス化
+  const transitLayer = new google.maps.TransitLayer()
   transitLayer.setMap(map)
 
   // マーカーを押したときにポップアップで出る情報
-  let contentString = '住所：千葉県浦安市舞浜１−１'
-  let infowindow = new google.maps.InfoWindow({content: contentString})
+  const contentString = '住所：千葉県浦安市舞浜１−１'
+  const infowindow = new google.maps.InfoWindow({ content: contentString })
 
-  //マーカーの実体を作る
-  let marker = new google.maps.Marker({position:location,map: map,title: contentString})
+  // マーカーの実体を作る
+  const marker = new google.maps.Marker({ position: location, map, title: contentString })
 
-  //マーカーをクリックできるようにする
-  marker.addListener('click', function() {
+  // マーカーをクリックできるようにする
+  marker.addListener('click', () => {
     infowindow.open(map, marker)
   })
 })
