ed9c1923a4452d8cfdbd65fe68e23162a5d0b742
sako kazuhiro <sako@rio.ne.jp>
2019-02-08 22:37:22 +0900

    wip

--- Gemfile
+++ Gemfile
@@ -51,6 +51,8 @@ gem 'bootstrap'
 gem 'jpmobile'
 
 gem 'webpacker', github: 'rails/webpacker'
+
+gem 'font-awesome-rails'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,4 +9,13 @@
 
 import 'src/application'
 import 'bootstrap/dist/js/bootstrap'
+import fontawesome from '@fortawesome/fontawesome'
+import faSolid from '@fortawesome/fontawesome-free-solid'
+import faRegular from '@fortawesome/fontawesome-free-regular'
+import faBrands from '@fortawesome/fontawesome-free-brands'
+
+fontawesome.library.add(faSolid, faRegular, faBrands)
+
+fontawesome.dom.i2svg()
+
 import './dentists.js'
