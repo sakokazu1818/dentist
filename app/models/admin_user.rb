d6c8c14f8d03bdaf8e437b20fb43e39dae271501
sako kazuhiro <sako@rio.ne.jp>
2019-03-06 16:10:55 +0900

    be rails generate model AdminUser

---
+++ app/models/admin_user.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class AdminUser < ApplicationRecord
+  has_secure_password
+
+  validates :name, :password_digest, presence: true
+  validates :name, uniqueness: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_050553) do
+ActiveRecord::Schema.define(version: 2019_03_06_070644) do
 
   create_table "admin_notifications", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "admin_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
