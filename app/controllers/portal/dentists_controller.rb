1528cc3bb99705ea9a3697bd502c12bd4c626bc8
sako kazuhiro <sako@rio.ne.jp>
2020-04-16 09:29:39 +0900

    s

--- app/controllers/portal/dentists_controller.rb
+++ app/controllers/portal/dentists_controller.rb
@@ -87,17 +87,20 @@ class Portal::DentistsController < ApplicationController
   end
 
   def set_contents
+    c_n = controller_name
     @contents = [
-      [
-        { text: '当院について', img_name: 'icon_clinic.png' },
-        { text: '院長紹介', img_name: 'icon_doctor.png' },
-        { text: '施設案内', img_name: 'icon_facilities-guide.png' },
-        { text: '初めての方へ', img_name: 'icon_medical_flow.png' },
-        { text: '訪問診察', img_name: 'icon_visit_consultation.png' },
-        { text: '診療時間、アクセス', img_name: ['icon_medical_hours.png',
-                                                'icon_access.png'] },
-        { text: '採用情報', img_name: 'icon_job.png' }
-     ]
+      { text: '当院について', img_name: 'icon_clinic.png',
+        link: c_n == 'abouts' ? '#' : portal_abouts_path },
+      { text: '院長あいさつ', img_name: 'icon_doctor.png',
+        link: c_n == 'director_introducings' ? '#' : portal_director_introducings_path },
+      { text: '施設案内', img_name: 'icon_facilities-guide.png',
+        link: c_n == 'facilities' ? '#' : portal_facilities_path },
+      { text: '初めての方へ', img_name: 'icon_medical_flow.png',
+        link: c_n == 'flow_examinations' ? '#' : portal_flow_examinations_path },
+      { text: '訪問診察', img_name: 'icon_visit_consultation.png',
+        link: c_n == 'visit_to_visits' ? '#' : portal_visit_to_visits_path },
+      { text: '採用情報', img_name: 'icon_job.png',
+        link: c_n == 'recruits' ? '#' : portal_recruits_path }
     ]
   end
 
