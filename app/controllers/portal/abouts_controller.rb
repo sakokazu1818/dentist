0182024aaca65d9f4553e635a6ca0320c45ce035
sako kazuhiro <sako@rio.ne.jp>
2020-05-06 19:05:13 +0900

    wip

--- app/controllers/portal/abouts_controller.rb
+++ app/controllers/portal/abouts_controller.rb
@@ -5,9 +5,13 @@ class Portal::AboutsController < ApplicationController
   # GET /portal/abouts.json
   def index
     @portal_abouts = []
-    return unless request.smart_phone?
 
-    set_contents_sp
-    render :index_smart_phone, layout: 'smart_phone.html.haml'
+    unless request.from_pc? || request.from_android_tablet? || request.from_ipad?
+      set_contents_sp
+      render :index_smart_phone, layout: 'smart_phone.html.haml'
+    else
+      set_contents
+      return
+    end
   end
 end
