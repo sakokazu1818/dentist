172f40d90fd25294f33e1e78f5cf248676b966dc
sako kazuhiro <sako@rio.ne.jp>
2020-05-07 20:17:17 +0900

    wip

--- app/javascript/packs/dentists.js
+++ app/javascript/packs/dentists.js
@@ -1,14 +1,12 @@
-$(() => {
+$(window).on('load', function() {
   const $body = $('body')
   if ($body.find('#admin').length > 0) {
     return
   }
 
-  let $bg_yellow = $($body).find('.bg-yellow-area');
-  if ($bg_yellow.length > 0) {
-    console.log($bg_yellow)
-    $($bg_yellow).css({'background-image':'url(' + $($body).find('#bg_yellow').attr('src') + ')'});
-  }
+  $('#fade').innerfade({speed: 2500, timeout: 6000 })
 
-  $('#fade').innerfade({speed: 2500, timeout: 6000 });
+  $('.clinic-img-wrapper .none').each(function(index, element){
+    $(this).removeClass('none')
+  })
 })
