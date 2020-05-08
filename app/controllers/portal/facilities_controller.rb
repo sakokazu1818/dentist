92ffa930cdc426b9dacc564a82c427419ea7db48
sako kazuhiro <sako@rio.ne.jp>
2020-05-08 10:30:53 +0900

    s

--- app/controllers/portal/facilities_controller.rb
+++ app/controllers/portal/facilities_controller.rb
@@ -6,10 +6,12 @@ class Portal::FacilitiesController < ApplicationController
   def index
     @portal_facilities = []
 
-    if request.smart_phone?
+    unless request.from_pc? || request.from_android_tablet? || request.from_ipad?
       set_contents_sp
-
       render :index_smart_phone, layout: 'smart_phone.html.haml'
+    else
+      set_contents
+      return
     end
   end
 end
