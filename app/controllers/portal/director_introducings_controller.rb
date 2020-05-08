89ec4d1ebb9c35ed902d385238c8637c4a8fa6ee
sako kazuhiro <sako@rio.ne.jp>
2020-05-08 09:40:42 +0900

    wip

--- app/controllers/portal/director_introducings_controller.rb
+++ app/controllers/portal/director_introducings_controller.rb
@@ -6,10 +6,12 @@ class Portal::DirectorIntroducingsController < ApplicationController
   def index
     @portal_director_introducings = []
 
-    if request.smart_phone?
+    unless request.from_pc? || request.from_android_tablet? || request.from_ipad?
       set_contents_sp
-
       render :index_smart_phone, layout: 'smart_phone.html.haml'
+    else
+      set_contents
+      return
     end
   end
 end
