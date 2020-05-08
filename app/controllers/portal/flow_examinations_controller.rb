4b0e3d6753039db3fd1c49bda7e69dd66f2c876a
sako kazuhiro <sako@rio.ne.jp>
2020-05-08 10:41:39 +0900

    s

--- app/controllers/portal/flow_examinations_controller.rb
+++ app/controllers/portal/flow_examinations_controller.rb
@@ -6,10 +6,12 @@ class Portal::FlowExaminationsController < ApplicationController
   def index
     @portal_flow_examinations = []
 
-    if request.smart_phone?
+    unless request.from_pc? || request.from_android_tablet? || request.from_ipad?
       set_contents_sp
-
       render :index_smart_phone, layout: 'smart_phone.html.haml'
+    else
+      set_contents
+      return
     end
   end
 end
