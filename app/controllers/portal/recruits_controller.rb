2e21c0f53c25cf6924a5e90592b2b6c3df4490ae
sako kazuhiro <sako@rio.ne.jp>
2020-05-08 10:51:55 +0900

    wip

--- app/controllers/portal/recruits_controller.rb
+++ app/controllers/portal/recruits_controller.rb
@@ -6,10 +6,12 @@ class Portal::RecruitsController < ApplicationController
   def index
     @portal_recruits = []
 
-    if request.smart_phone?
+    unless request.from_pc? || request.from_android_tablet? || request.from_ipad?
       set_contents_sp
-
       render :index_smart_phone, layout: 'smart_phone.html.haml'
+    else
+      set_contents
+      return
     end
   end
 end
